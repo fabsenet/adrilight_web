041cf26ef3768fc09b35a3c7a635c2fb36adc895
Fabian Wetzel <fabsenet@googlemail.com>
2018-10-26 16:46:16 +0200

    initial commit

---
+++ Controllers/ReleaseInfoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using adrilight_web.Models;
+
+namespace adrilight_web.Controllers
+{
+    public class ReleaseInfoController : Controller
+    {
+        public IActionResult Index(string version, string lang)
+        {
+            string view = null;
+            switch (version)
+            {
+                case "2.0.7":
+                    if (lang.StartsWith("de", StringComparison.OrdinalIgnoreCase))
+                    {
+                        view = "v2.0.7_de";
+                    }
+                    else
+                    {
+                        //en is default
+                        view = "v2.0.7_en";
+                    }
+                    break;
+            }
+            if (view != null)
+            {
+                ViewBag.Version = version;
+                return View(view);
+            }
+
+            return this.NotFound("This version does not exist (yet).");
+        }
+    }
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.Content("nothing here for now");
+        }
+    }
+}
